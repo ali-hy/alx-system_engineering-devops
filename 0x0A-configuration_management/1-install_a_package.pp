cc59439acfbf046bee83b30801a169bdcd3ae5cd
ali-hy <ali.haitham.ymy@gmail.com>
2024-04-19 23:45:53 +0200

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 # installs the Flask package using pip3
-package { 'flask':
+package { 'Werkzeug ':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
